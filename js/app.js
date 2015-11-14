7c64980bce173303e2e6f7555cc5f73e56a46ad1
virgilvox <moheeb.zara@citrix.com>
2015-11-13 22:37:01 -0700

    basic

---
+++ js/app.js
@@ -0,0 +1,51 @@
+
+var app = angular.module('MyApp');
+
+app.controller('AppCtrl', function($scope) {
+
+
+  $scope.payload = function(data){
+    $scope.displayText = data.payload.text;
+    $scope.$apply()
+  }
+
+
+  var GET = {};
+  var query = window.location.search.substring(1).split("&");
+  for (var i = 0, max = query.length; i < max; i++)
+  {
+    if (query[i] === "")
+    continue;
+    var param = query[i].split("=");
+    GET[decodeURIComponent(param[0])] = decodeURIComponent(param[1] || "");
+  }
+
+  var conn = meshblu.createConnection({
+    "uuid": GET.uuid,
+    "token": GET.token
+  });
+
+  var MESSAGE_SCHEMA = {
+    "type": 'object',
+    "properties": {
+      "text": {
+        "type": "string"
+      }
+    }
+  };
+
+  conn.on('ready', function(data){
+    console.log('UUID AUTHENTICATED!');
+    console.log(data);
+    conn.update({
+      "uuid": GET.uuid,
+      "messageSchema": MESSAGE_SCHEMA
+    });
+
+    conn.on('message', function(data){
+      $scope.payload(data);
+    });
+
+  });
+
+});
